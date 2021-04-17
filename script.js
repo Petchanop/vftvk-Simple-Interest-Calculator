ab360d35f020dc4ebcc60030732cf74cbe1f0e0f
Petchanop <69717257+Petchanop@users.noreply.github.com>
2021-04-18 02:05:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function compute()
     //use span class highlight for highlight text
     document.getElementById("result").innerHTML= 
     "<br>If you deposit "+ "<span class='highlight'>" +principal + "</span>" +
-    ",<br>at an interest rate of "+ "<span class='highlight'>" + rate + "</span>" +
-    "%.<br>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>" +
+    ",<br>at an interest rate of "+ "<span class='highlight'>" + rate + "%."+ "</span>" +
+    "<br>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>" +
     ",<br>in the year " + "<span class='highlight'>" + year + "</span>";
 }
 
